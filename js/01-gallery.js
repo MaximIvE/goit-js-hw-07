ac8ac8f86aeb8b99008fe97d8fda8513ad322e32
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-25 20:14:16 +0300

    add gallery for task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+// console.log(galleryEl);
+
+// const element = galleryItems[0];
+
+function createEl({original, preview, description}){
+   return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>`;
+};
+
+const collection = galleryItems.map((element)=>{
+    return createEl(element);
+}).join("");
+
+console.log(collection);
+galleryEl.insertAdjacentHTML("beforeend", collection);
