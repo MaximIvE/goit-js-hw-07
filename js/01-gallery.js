de95622d8a858546bdb4dca5d94baecde5b411ea
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-25 21:36:27 +0300

    adds event delegation to button and filter for task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,7 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-// console.log(galleryItems);
-
+// #1 Додаємо до галереї розмітку картинок
 const galleryEl = document.querySelector(".gallery");
-// console.log(galleryEl);
-
-// const element = galleryItems[0];
 
 function createEl({original, preview, description}){
    return `<div class="gallery__item">
@@ -25,5 +20,26 @@ const collection = galleryItems.map((element)=>{
     return createEl(element);
 }).join("");
 
-console.log(collection);
 galleryEl.insertAdjacentHTML("beforeend", collection);
+
+// #2 Створюємо слухача події клік та робимо делегування з фільтром
+
+const imageEl = document.querySelector(".gallery__image");
+
+galleryEl.addEventListener('click', e => {
+    e.preventDefault();
+    const currentItem = e.target;
+    if (currentItem.className !== "gallery__image"){
+        return;
+    };
+
+    viewPicture(currentItem);
+});
+
+function viewPicture(image){
+    console.log("Спрацювала функція viewPicture");
+    console.log(image);
+};
+
+
+
