8e0176bb387a1da03e6be337dbd51b5ef46abfb3
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-26 23:46:42 +0300

    Task2. Subtask 1. Creates dynamic gallery markup.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+
+function createEl(src, data, alt){
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${data}">
+      <img
+        class="gallery__image"
+        src="${src}"
+        data-source="${data}"
+        alt="${alt}"
+      />
+    </a>
+  </div>`
+};
+
+const galleryString = galleryItems.reduce((contanier, galleryItem) => {
+  const {preview, original, description} = galleryItem;
+  
+  contanier = contanier + createEl(preview, original, description);
+  return contanier;
+}, "");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryString);
+
+
